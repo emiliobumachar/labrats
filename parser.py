2c4b66f8ac1e779e599e669a6edbc4c122217c49
emiliobumachar <emiliobumachar@gmail.com>
2015-10-04 11:07:41 -0300

    parser for network messages as drafted. Should work for both bank and atm. Tested in python 3.

---
+++ parser.py
@@ -0,0 +1,20 @@
+def parse(msgPayload):
+	'''Returns a dictionary with the field titles as keys. Raises an error if signature does not match, decryption fails, or invalid field.'''
+	validTitles=['atmID',
+                     'msgCounter',
+                     'replyTo',
+                     'bankAns',
+                     'action',
+                     'atmAns',
+                     '$',
+                     'account']
+	def checkSignature(blob):
+		return blob
+	def decrypt(blob):
+		return blob
+	plainText=decrypt(checkSignature(msgPayload))
+	fields=plainText.split()
+	fieldsTuples=[f.split('=') for f in fields]
+	fieldsDict = {ft[0]:ft[1] for ft in fieldsTuples}
+	assert all(t in validTitles for t in fieldsDict)
+	return fieldsDict
