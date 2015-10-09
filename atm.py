f14625650e127c7acb8520dc6c2b86602c7dc535
emiliobumachar <emiliobumachar@gmail.com>
2015-10-09 01:50:31 -0300

    atm sends hardcoded message to bank on account creation, bank accepts it.

--- atm.py
+++ atm.py
@@ -1,6 +1,9 @@
 import socket
+from common import *
+
 
 balance=10.00
+atmID=str(rand.randint(0,1e12))
 
 #TODO get operation from input
 print("    RatLABS ATM    ")
@@ -10,43 +13,50 @@ print("""
 2)        Withdraw
 3)        Deposit
 4)        Quit
+5)		  New Account
 
 
 """)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('127.0.0.11', 3000)) #TODO read ip and port
 Option=int(input("Enter Option: "))
 
 if Option == 0:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(('127.0.0.1', 3000)) #TODO read ip and port
-    s.send('tcp socket test')
-    data = s.recv(1024) #TODO define limit
-    s.close()
-    print data
+	
+	s.send('tcp socket test')
+	data = s.recv(1024) #TODO define limit
+	s.close()
+	print data
 
 if Option==1:
-    print("Balance   ",balance)
+	print("Balance   ",balance)
 
 
 if Option==2:
-    print("Balance    ",balance)
-    Withdraw=float(input("Enter Withdraw amount  "))
-    if Withdraw>0:
-        forewardbalance=(balance-Withdraw)
-        print("Foreward Balance   ",forewardbalance)
-    elif Withdraw>balance:
-        print("No funs in account")
-    else:
-        print("None withdraw made")
+	print("Balance    ",balance)
+	Withdraw=float(input("Enter Withdraw amount  "))
+	if Withdraw>0:
+		forewardbalance=(balance-Withdraw)
+		print("Foreward Balance   ",forewardbalance)
+	elif Withdraw>balance:
+		print("No funs in account")
+	else:
+		print("None withdraw made")
 
 if Option==3:
-    print("Balance   ",balance)
-    Deposit=float(input("Enter deposit amount  "))
-    if Deposit>0:
-        forewardbalance=(balance+Deposit)
-        print("Forewardbalance   ",forewardbalance)
-    else:
-        print("None deposit made")
-
+	print("Balance   ",balance)
+	Deposit=float(input("Enter deposit amount  "))
+	if Deposit>0:
+		forewardbalance=(balance+Deposit)
+		print("Forewardbalance   ",forewardbalance)
+	else:
+		print("None deposit made")
+if Option==5:
+	text='atmID='+atmID+' action=n atmAns=y $=42.00 account=SomeGuy'
+	sendPlainText(s, text)
 
 if Option==4:
-    exit()
+	exit()
+	
+
--- bank.py
+++ bank.py
@@ -1,60 +1,55 @@
 import argparse
 import socket
-import parser
-import string
-import random
 import os
-rand=random.SystemRandom()
-LENGTH_OF_ALL_PLAINTEXTS = 333
+from common import *
+
+
 authFileName="bank.auth" #todo: get this name as a parameter
 
-class ret255(Exception):
-	pass
 class Bank:
 
 	def __init__(self):
 		self.knownAtms={} #{atmID-as-string:{'incoming':latest-incoming-couter-as-string, 'outgoing':latest-outgoing-couter-as-string} for each atm that ever sent a message}
 		secretKey=rand.randint(0,2**256-1)
 		if os.path.isfile(authFileName):
-			raise ret255
+			debug('File already exists')
+			#raise ret255
 		with open (authFileName,'w') as authFile:
 			authFile.write(str(secretKey))
-		
+		self.actionList={'n':self.treatNewAccount,
+				 'd':self.treatDeposit,
+				 'w':self.treatWithdrawal,
+				 'g':self.treatGetBalance}
 		self.accountHolders={} #{account:balance for every account}
 		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.listen2network()
-		self.actionList={'n':self.treatNewAccount,
-						 'd':self.treatDeposit,
-						 'w':self.treatWithdrawal,
-						 'g':self.treatGetBalance}
+		
 
 	def listen2network(self):
-		self.s.bind(('127.0.0.1', 3000))
+		self.s.bind(('127.0.0.11', 3000))
 		self.s.listen(1)
 
 		while 1:
-			c = self.s.accept()
-			cli_conn, cli_addr = c
+			self.c = self.s.accept()
+			self.cli_conn, self.cli_addr = self.c
 
-			data = cli_conn.recv(1024) # check buffer limit
-			print 'data:', data
+			data = self.cli_conn.recv(1024) # check buffer limit
+			debug( 'data:'+data)
 
-			data = 'received: ' + data
-			#treatMessage(self,cli_conn.?)
+			#data = 'received: ' + data
+			self.treatMessage(data)
 
-			cli_conn.send(data)
-			cli_conn.close()
+			#self.cli_conn.send(data)
+			self.cli_conn.close()
 			
 	def sendReply(self, bankAnswer):
 		replyText=('atmID='+self.fieldsDict['atmID']+
-				   ' msgCounter='+self.knownAtms[self.fieldsDict['atmID']]['outgoing']+
-				   ' replyTo='+self.fieldsDict['msgCounter']+
+				   #' msgCounter='+self.knownAtms[self.fieldsDict['atmID']]['outgoing']+
+				   #' replyTo='+self.fieldsDict['msgCounter']+
 				   ' bankAns='+{True:'y',False:'n'}[bankAnswer])
 		if self.fieldsDict['action']=='g':
 			replyText=replyText+' $='+str(self.accountHolders[self.fieldsDict['account']])
-		replyText=replyText+' pad='
-		replyText=replyText+rand.choice(string.ascii_letters)*(LENGTH_OF_ALL_PLAINTEXTS-len(replyText))
-		#self.cli_sock.(replyText)
+		sendPlainText(self.cli_conn, replyText)
 		self.knownAtms[self.fieldsDict['atmID']]['outgoing']=str(1+int(self.knownAtms[self.fieldsDict['atmID']]['outgoing']))
 		
 	def treatNewAccount(self):
@@ -93,11 +88,13 @@ class Bank:
 			print('{"account":"'+self.fieldsDict['account']+'","balance":'+str(self.accountHolders[self.fieldsDict['account']])+'}')
 	
 	def treatMessage(self, message):
-		self.fieldsDict=parser.parse(message)
-		if self.fieldsDict['atmID'] in self.knownAtms:#not first contact
-			if int(self.fieldsDict['msgCounter'])<=int(self.knownAtms[self.fieldsDict['atmID']]['incoming']): #replay attack or reorder attack!
-				return #ignore offending message
-		self.knownAtms[self.fieldsDict['atmID']]={'incoming':self.fieldsDict['msgCounter'],'outgoing':'0'}
+		self.fieldsDict=msgParse(message)
+		# delete this code if messageID is revoked, uncomment otherwise
+		# if self.fieldsDict['atmID'] in self.knownAtms:#not first contact
+			# if int(self.fieldsDict['msgCounter'])<=int(self.knownAtms[self.fieldsDict['atmID']]['incoming']): #replay attack or reorder attack!
+				# return #ignore offending message
+		#self.knownAtms[self.fieldsDict['atmID']]={'incoming':self.fieldsDict['msgCounter'],'outgoing':'0'}
+		self.knownAtms[self.fieldsDict['atmID']]={'incoming':0,'outgoing':'0'}
 		assert('action' in self.fieldsDict)
 		assert self.fieldsDict['action'] in self.actionList
 		self.actionList[self.fieldsDict['action']]()
